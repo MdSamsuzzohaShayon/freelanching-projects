2ed29e1a479a441e8238169e738c1cd9bca0ca3d
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-12-07 21:22:45 +0600

    skip button add  but some problem with colors

--- js/main.js
+++ js/main.js
@@ -19,7 +19,12 @@
 
 
 // let lifeTopic = [health, career, love, spirituality, family, money, fun, friends];
-let lifeTopic =  document.querySelectorAll(".life-topic");
+const lifeTopic = document.querySelectorAll(".life-topic");
+const skip = document.getElementById("skip");
+console.log("skip :", skip);
+
+const date = document.getElementById("date");
+console.log("Date :", date);
 
 
 // DISPLAY LIFE TOPIC ONE BY ONE 
@@ -34,41 +39,103 @@ lifeTopic[currentTopic].style.display = 'block';
 
 
 
+
 // ADD EVENT LISTENER FOR ALL MARK 1 TO 10 AND EXTRACTING THE VALUE 
 const mark = document.querySelectorAll(".mark");
 console.log("Mark :", mark);
-mark.forEach((m, index) => {
-    console.log("index of mark: ", index);
-    m.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log("index of mark: ", index);
-        console.log("event value: ", e.target.childNodes[0].textContent);
-        const markValue = e.target.childNodes[0].textContent;
-        console.log("mark value: ", markValue);
-        // if(markValue == 2){
-        //     console.log("Mark value is 2");
-        // }
-        console.log("life topic: ", lifeTopic.length);
-        console.log("Current: ", currentTopic);
-        lifeTopic[currentTopic].style.display = 'none';
-        if (currentTopic >= lifeTopic.length -1) {
-            // BLOCK SOME AREA AND SHOW FINAL DISPLAY 
-            console.log("no more questuon");
-        } else {
-            currentTopic++;
-            lifeTopic[currentTopic].style.display = 'block';
-        }
-    })
-    // console.log(m.childNodes[0].textContent);
+
+
+
+
+
+// INITIAL EMPTY ARRAY, GETTING VALUE FROM PROMPT AND SET TO GRAPH 
+let labels = [];
+let data = [];
+let bgColors = [];
+let borderColors = [];
+// COLOR ARRAY FOR ONE DIRRERENT DATA AND LEVEL 
+let colorArray = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+
+
+
+
+let addElement = true;
+// if(addElement){}
+
+
+// skip.addEventListener("")
+
+
+skip.addEventListener("click", e => {
+    e.preventDefault();
+    lifeTopic[currentTopic].style.display = 'none';
+    currentTopic++;
+    console.log("Current: ", currentTopic);
+    lifeTopic[currentTopic].style.display = 'block';
+    addElement = false;
 });
 
+if (addElement == true) {
+    mark.forEach((m, index) => {
+
+        console.log("index of mark: ", index);
+        m.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log("index of mark: ", index);
+            // console.log("event value: ", e.target.childNodes[0].textContent);
+            const markValue = e.target.childNodes[0].textContent;
+            console.log("mark value: ", markValue);
+            // if(markValue == 2){
+            //     console.log("Mark value is 2");
+            // }
+            console.log("life topic: ", lifeTopic.length);
 
 
-const skip = document.getElementById("skip");
-console.log("skip :", skip);
+            console.log("Current: ", currentTopic);
+            lifeTopic[currentTopic].style.display = 'none';
+            if (currentTopic >= lifeTopic.length - 1) {
+                // BLOCK SOME AREA AND SHOW FINAL DISPLAY 
+                // MAKE BIGGER CHART 
+                console.log("no more questuon");
+            } else {
 
-const date = document.querySelectorAll("date");
-console.log("Date :", date);
+                // CHECK WHICH TOPIC WE ARE ON 
+                console.log("Current life topic question : ", lifeTopic[currentTopic].textContent);
+                // ADD THIS LIFE TOPIC TO GRAPH LABEL 
+                labels.push(lifeTopic[currentTopic].textContent);
+                console.log("labels array: ", labels);
+                // ADD VALUE OF LIFE TOPIC TO DATA 
+                data.push(markValue);
+                console.log("data array: ", data);
+                bgColors.push(colorArray[currentTopic]);
+                console.log("bg color of array: ", colorArray[currentTopic]);
+                bgColors.push(colorArray[currentTopic]);
+                console.log("border color of array: ", colorArray[currentTopic]);
+                drawGraph();
+
+                currentTopic++;
+                lifeTopic[currentTopic].style.display = 'block';
+            }
+        })
+        // console.log(m.childNodes[0].textContent);
+    });
+}
+
+
+
+
+
+
 
 
 // PROMPT JAVASCRIPT ENDS 
@@ -93,20 +160,23 @@ function drawGraph() {
         type: 'polarArea',
         data: {
             // CONGE LABEL OF COLORS AND BE AWARE OF HOW MANY COLORS NEED 
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels,
             datasets: [
                 {
                     label: '# of Votes',
                     // DYNAMICALLY CHANGE THE VALUE OF DATA 
-                    data: [10, 9, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
+                    data,
+                    backgroundColor: bgColors
+                    // [
+                    //     // ADD COLOR DYNAMICALLY 
+                    //     'rgba(255, 99, 132, 0.2)',
+                    //     'rgba(54, 162, 235, 0.2)',
+                    //     'rgba(255, 206, 86, 0.2)',
+                    //     'rgba(75, 192, 192, 0.2)',
+                    //     'rgba(153, 102, 255, 0.2)',
+                    //     'rgba(255, 159, 64, 0.2)'
+                    // ]
+                    ,
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
@@ -155,6 +225,8 @@ function drawGraph() {
         }
     });
 }
+
+
 
 
 // CHART JS POLAR AREA CHART START 
